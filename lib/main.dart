47807215586398284332faa2d7f8a66477fdd23b
dariomatias-dev <matiasdario75@gmail.com>
2026-05-26 19:26:05 -0300

    feat(lib/main.dart): added `SharedPreferences` instance injection
    - Implemented `SharedPreferences` instance definition for resource injection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:catch_the_cat/core/providers/shared_preferences_provider.dart';
 import 'package:catch_the_cat/core/theme/app_theme.dart';
 
 import 'package:catch_the_cat/features/game/presentation/screens/game_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: App()));
+
+  final prefs = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+      child: const App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
