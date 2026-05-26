8faa330d163153812de7756a8affc026390c5724
dariomatias-dev <matiasdario75@gmail.com>
2026-05-26 19:41:58 -0300

    feat(lib): created root widget
    - Developed application root widget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:catch_the_cat/src/core/providers/shared_preferences_provider.dart';
-import 'package:catch_the_cat/src/core/theme/app_theme.dart';
+import 'package:catch_the_cat/src/catch_the_cat_app.dart';
 
-import 'package:catch_the_cat/src/features/game/presentation/screens/game_screen.dart';
+import 'package:catch_the_cat/src/core/providers/shared_preferences_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,22 +14,7 @@ Future<void> main() async {
   runApp(
     ProviderScope(
       overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
-      child: const App(),
+      child: const CatchTheCatApp(),
     ),
   );
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Catch the Cat',
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      darkTheme: AppTheme.dark,
-      home: const GameScreen(),
-    );
-  }
 }
---
+++ lib/src/catch_the_cat_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:catch_the_cat/src/core/theme/app_theme.dart';
+
+import 'package:catch_the_cat/src/features/game/presentation/screens/game_screen.dart';
+
+class CatchTheCatApp extends StatelessWidget {
+  const CatchTheCatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Catch the Cat',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: AppTheme.dark,
+      home: const GameScreen(),
+    );
+  }
+}
